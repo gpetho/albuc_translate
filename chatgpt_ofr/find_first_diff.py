b61f69a92c39351fe798e3881730fa95bf0a4f9f
gpetho <pagstudium@gmail.com>
2024-11-10 19:35:19 +0100

    add ofr alignment

--- chatgpt_ofr/find_first_diff.py
+++ chatgpt_ofr/find_first_diff.py
@@ -7,7 +7,7 @@ with open(sys.argv[2]) as f:
     lines2 = f.readlines()
 
 for i, (l1, l2) in enumerate(zip(lines, lines2)):
-    if l1[:10] != l2[:10]:
+    if l1[:7] != l2[:7]:
         print(i)
         print(l1)
         break

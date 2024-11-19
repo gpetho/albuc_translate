f3e00c8ec22a25f24566d7e4ddac41d321d24483
gpetho <pagstudium@gmail.com>
2024-11-19 17:53:12 +0100

    update path

--- check_aligned_completeness.sh
+++ check_aligned_completeness.sh
@@ -1,4 +1,4 @@
-error_found=false; while IFS= read -r file; do [ "$(wc -l < "$file")" -ne 2806 ] && echo "File $file does not have exactly 2806 lines. It has $(wc -l < "$file") lines." && error_found=true; done < <(find aligned_sl_mt -type f); [ "$error_found" = false ] && echo "All files in the aligned_sl_mt directory and its subdirectories have exactly 2806 lines."
+error_found=false; while IFS= read -r file; do [ "$(wc -l < "$file")" -ne 2806 ] && echo "File $file does not have exactly 2806 lines. It has $(wc -l < "$file") lines." && error_found=true; done < <(find aligned_sl_occ -type f); [ "$error_found" = false ] && echo "All files in the aligned_sl_occ directory and its subdirectories have exactly 2806 lines."
 
 error_found=false; while IFS= read -r file; do [ "$(wc -l < "$file")" -ne 2173 ] && echo "File $file does not have exactly 2173 lines. It has $(wc -l < "$file") lines." && error_found=true; done < <(find aligned_sl_ara -type f); [ "$error_found" = false ] && echo "All files in the aligned_sl_ara directory and its subdirectories have exactly 2173 lines."
 
